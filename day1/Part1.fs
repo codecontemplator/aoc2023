c4b3bd2f93b4c154fc8a269fc3ef3861f5ab2ea4
daniel.brannstrom <daniel.brannstrom.ext@pengvin.se>
2023-12-01 09:10:36 +0100

    day1

---
+++ day1/Part1.fs
@@ -0,0 +1,16 @@
+﻿module Part1
+
+open System.IO
+open System
+
+let getNumber s =
+    let digits = s |> Seq.filter Char.IsDigit 
+    let firstDigit = digits |> Seq.head
+    let lastDigit = digits |> Seq.rev |> Seq.head
+    int <| $"{firstDigit}{lastDigit}"
+    
+
+let lines = File.ReadAllLines("input.txt")
+let r = lines |> Seq.map getNumber |> Seq.sum
+
+printf "%A" r
---
+++ day1/Part2.fs
@@ -0,0 +1,41 @@
+﻿module Part2
+
+open System.Text.RegularExpressions
+open System.IO
+open System
+open System.Diagnostics
+
+let tokenToDigit token =
+    match token with 
+    | "one" -> "1"
+    | "two" -> "2"
+    | "three" -> "3"
+    | "four" -> "4"
+    | "five" -> "5"
+    | "six" -> "6"
+    | "seven" -> "7"
+    | "eight" -> "8"
+    | "nine" -> "9"
+    | _ -> 
+        Debug.Assert(token.Length = 1)
+        Debug.Assert(Char.IsDigit(token[0]))
+        token
+
+let revString x = new string(x |> Seq.rev |> Seq.toArray)
+
+let getNumber s =
+
+    let digits = ["one";"two";"three";"four";"five";"six";"seven";"eight";"nine"];
+    let digitsRev = digits |> List.map revString
+
+    let regExFirst = "\d|" + String.Join('|',digits)
+    let firstDigit = Regex.Match(s, regExFirst).Value |> tokenToDigit
+    let regExLast = "\d|" + String.Join('|',digitsRev)
+    let lastDigit = Regex.Match(revString s, regExLast).Value |> revString |> tokenToDigit
+
+    int <| $"{firstDigit}{lastDigit}"
+
+let lines = File.ReadAllLines("input.txt")
+let r = lines |> Array.map getNumber |> Seq.sum
+
+printf "part2: %A" r
