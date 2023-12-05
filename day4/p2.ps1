a75e1bec90584e6399b1ad1931ab875c489053aa
daniel.brannstrom <daniel.brannstrom.ext@pengvin.se>
2023-12-05 18:21:54 +0100

    d4 p2

--- day4/p2.ps1
+++ day4/p2.ps1
@@ -3,6 +3,8 @@ using namespace System.Collections.Generic
 cd $PSScriptRoot
 $rows = Get-Content "data.txt"
 [int64]$sum = 0
+$ri = 0
+$cardCount = $rows | % { 1 }
 foreach ($row in $rows) {
     $row -match "^Card\s+(\d+):(.*?)\|(.*)$" | Out-Null
     $wins = $Matches[2].Trim() -split "\s+" | % { [int]$_ }
@@ -11,10 +13,15 @@ foreach ($row in $rows) {
     $mineSet = [HashSet[int]]::new([int[]]$mine)
     $intersection = [HashSet[int]]::new($winsSet)
     $intersection.IntersectWith($mineSet)
-    $count = $intersection.Count 
-    if ($count -gt 0) {
-        $sum += [Math]::Pow(2, $count - 1)
-    }
+    $count = $intersection.Count
+    for($i = 0; $i -lt $count; $i++) {
+        $nr = $i + $ri + 1
+        if ($nr -lt $rows.Count) {
+            $cardCount[$nr] = $cardCount[$nr] + $cardCount[$ri];
+        }
+    } 
+    $ri++
 }
 
+$sum = $cardCount | Measure-Object -sum | Select-Object -ExpandProperty Sum
 Write-Host $sum
